0e2ae5a9a5af34def28b99374f255503cf09db52
Leirbag <gabrielelfassi@yahoo.fr>
2022-04-26 14:56:07 +0200

    Class name change + vested system added

--- contracts/Presales.sol
+++ contracts/Presales.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Greeter is Ownable {
+contract Presales is Ownable {
     using SafeMath for uint256;
 
     // distributed token
@@ -14,90 +14,156 @@ contract Greeter is Ownable {
     // payment token
     IERC20 private paymentToken;
     // numerator
-    uint256 private num;
+    uint256 public num;
     // numerator
-    uint256 private den;
+    uint256 public den;
     // deadline for applying to the presales
-    uint256 private deadline;
+    uint256 public deadline;
+    // vested endtime (timestamp)
+    uint256 public vestedTime;
+    // claim launchtime
+    uint256 public launchtime;
     // refund trigger bool
-    bool private isRFEnabled;
+    bool public isRFEnabled;
     // keep ownership on opening claim token
-    bool private isClaimOpened;
+    bool public isClaimOpen;
 
     struct User {
         uint256 allocAmount;
         uint256 paidAmount;
+        uint256 tokenAmount;
+        uint256 lastClaim;
         bool isWL;
     }
     // allocation mapping per address
     mapping(address=>User) private users;
+
+    modifier onlyWL {
+        require(users[msg.sender].isWL, "User is not whitelisted");
+        _;
+    }
+
+    event PrivateSaleAttendance(address indexed address_, uint256 amount_);
 
     constructor(
         IERC20 presaleToken_,
         IERC20 paymentToken_,
         uint256 deadline_,
         uint256 num_,
-        uint256 den_) {
+        uint256 den_,
+        uint256 vestedTime_) {
         presaleToken = presaleToken_;
         paymentToken = paymentToken_;
+        vestedTime = vestedTime_;
         deadline = deadline_;
         num = num_;
         den = den_;
-        isClaimOpened = true;
     }
 
-    function participate(uint256 amount_) external {
+    function privateSale(uint256 amount_) external onlyWL {
         require(block.timestamp < deadline, "Presale is closed.");
         require(amount_ > 0, "The minimum amount must be greater than 0.");
-        require(users[msg.sender].isWL, "User is not whitelisted.");
-        require(users[msg.sender].allocAmount >= amount_, "User has not enough allocation.");
-        require(users[msg.sender].paidAmount > 0, "User has not enougth allocation.");
-        require(users[msg.sender].allocAmount > users[msg.sender].paidAmount.add(amount_), "You exceeded the authorized amount");
+        require(users[msg.sender].allocAmount >= users[msg.sender].paidAmount.add(amount_), "You exceeded the authorized amount");
         paymentToken.transferFrom(msg.sender, address(this), amount_);
         users[msg.sender].paidAmount  += amount_;
+        users[msg.sender].tokenAmount += amount_.mul(num).div(den);
+        emit PrivateSaleAttendance(msg.sender, amount_);
     }
 
     function claim() external {
-        require(users[msg.sender].isWL, "User is not whitelisted.");
-        require(block.timestamp >= deadline, "You can not claim your token yet.");
-        require(isClaimOpened, "Claim is not open yet.");
-        uint256 tokenAmount = users[msg.sender].paidAmount.mul(num).div(den);
-        presaleToken.approve(address(this), tokenAmount);
-        presaleToken.transferFrom(address(this), msg.sender, tokenAmount);
-        users[msg.sender].isWL = false;
+        require(isClaimOpen, "Claim is not open yet.");
+        uint256 tokenAmount = getVestedAmountToClaim(users[msg.sender].tokenAmount);
+        presaleToken.approve(address(this), users[msg.sender].tokenAmount);
+        presaleToken.transferFrom(
+            address(this),
+            msg.sender,
+            tokenAmount
+        );
+        users[msg.sender].lastClaim = block.timestamp;
+        users[msg.sender].tokenAmount -= tokenAmount;
     }
 
-    function refund() external {
+    function refund() external onlyWL {
         require(isRFEnabled, "Refund is not enabled yet.");
-        require(users[msg.sender].isWL, "User is not whitelisted.");
         require(users[msg.sender].paidAmount > 0, "The minimum amount must be greater than 0.");
         paymentToken.approve(address(this),users[msg.sender].paidAmount);
         paymentToken.transferFrom(address(this), msg.sender,users[msg.sender].paidAmount);
-        users[msg.sender] = User(0,0,false);
+        users[msg.sender] = User(0,0,0,0,false);
     }
 
     // START: view functions
-    function getPresaleToken_() external view returns(IERC20) {
+    function getPresaleToken() external view returns(IERC20) {
         return presaleToken;
+    }
+
+    function getPaymentToken() external view returns(IERC20) {
+        return paymentToken;
+    }
+    
+    function getPresaleDeadline() external view returns(uint256) {
+        return deadline;
+    }
+
+    function getUser(address address_) external view returns(User memory) {
+        return users[address_];
+    }
+
+    function getVestedAmountToClaim(uint256 amount_) public view returns(uint256) {
+        uint256 startTime = users[msg.sender].lastClaim;
+        if (startTime == 0) {
+            startTime = launchtime;
+        } else if (startTime >= vestedTime) {
+            startTime = vestedTime;
+        }
+        uint256 rps = amount_.div(vestedTime.sub(startTime));
+        uint256 currentTime = block.timestamp;
+        if (currentTime >= vestedTime) {
+            currentTime = vestedTime;
+        }
+        return rps.mul(currentTime.sub(startTime));
     }
     // END: view functions
 
     // START: admin
+    /**
+     * Withdraw funds. If `all_` is set to `true` then all funds will be withdrawn
+     * @param amount_ is the amount user wants to withdraw
+     * @param all_ is a boolean value to withdraw all funds
+     */
+    function __withdraw(uint256 amount_, bool all_) external onlyOwner {
+        require(block.timestamp >= deadline, "You can not withdraw funds yet.");
+        paymentToken.approve(address(this), paymentToken.balanceOf(address(this)));
+        uint256 tmpAmount = amount_;
+        if (all_) {
+            tmpAmount = paymentToken.balanceOf(address(this));
+        }
+        paymentToken.transferFrom(address(this),owner(), tmpAmount);
+    }
+
     /**
      * Updates the allocation of an address
      * @param address_ Address of the user who will receive the allocation
      * @param amount_ Amount of the allocation
      */
     function __updateWLAllocation(address address_, uint256 amount_, bool isWL_) external onlyOwner {
-        users[address_] = User(amount_, 0, isWL_);
+        users[address_] = User(amount_, 0,0,0,isWL_);
     }
 
+    /**
+     * Add batch of whitelisted addresses
+     * @param addresses_ is a list of addresses to wl
+     * @param amount_ is the amount of allocation
+     */
     function __addWLs(address[] memory addresses_, uint256 amount_) external onlyOwner {
         for(uint256 i=0;i<addresses_.length;i++){
-            users[addresses_[i]] = User(amount_, 0, true);
+            users[addresses_[i]] = User(amount_, 0,0,0, true);
         }
     }
 
+    /**
+     * Dis/Enable the refund function
+     * @param isRFEnabled_ is the value (true: enable | false: disable)
+     */
     function __setEnabledRefund(bool isRFEnabled_) external onlyOwner {
         isRFEnabled = isRFEnabled_;
     }
@@ -106,8 +172,21 @@ contract Greeter is Ownable {
         deadline = deadline_;
     }
     
-    function __setIsClaimOpened(bool isClaimOpened_) external onlyOwner {
-        isClaimOpened = isClaimOpened_;
+    function __setIsClaimOpen(bool isClaimOpen_) external onlyOwner {
+        require(vestedTime > block.timestamp, "Vested end time should be higher than current time.");
+        isClaimOpen = isClaimOpen_;
+        launchtime = block.timestamp;
+    }
+    
+    function __setVestedTime(uint256 vestedTime_) external onlyOwner {
+        vestedTime = vestedTime_;
+    }
+
+    function __setTokens(IERC20 paymentToken_, IERC20 presaleToken_)
+    external
+    onlyOwner {
+        paymentToken = paymentToken_;
+        presaleToken = presaleToken_;
     }
     // END: admin
 }
