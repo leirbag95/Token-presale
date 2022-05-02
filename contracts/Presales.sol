37b886a940cd3477ee825b64ee4cffb2cf007285
Leirbag <gabrielelfassi@yahoo.fr>
2022-05-02 11:44:24 +0200

    nonReentrant class added and opening of the public sales automatically deactivated.

--- contracts/Presales.sol
+++ contracts/Presales.sol
@@ -2,29 +2,26 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Presales is Ownable {
+contract Presales is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     // distributed token
     IERC20 private presaleToken;
     // payment token
     IERC20 private paymentToken;
+    // hardcap to reach
+    uint256 public hardcap;
     // private sale num
-    uint256 public priNum;
+    uint256 public num;
     // private sale den
-    uint256 public priDen;
-    // public sale num
-    uint256 public pubNum;
-    // public sale den
-    uint256 public pubDen;
-    // max per wallet
+    uint256 public den;
+    // max token per wallet
     uint256 public MAX_WALLET;
-    // max cap to raised
-    uint256 public MAX_CAP;
     // current cap
     uint256 public currentCap;
     // deadline for applying to the presales
@@ -39,6 +36,8 @@ contract Presales is Ownable {
     bool public isRFEnabled;
     // keep ownership on opening claim token
     bool public isClaimOpen;
+    // open public sales
+    bool public isPublicSalesOpen;
     // funds receiver address
     address public receiver;
 
@@ -47,13 +46,12 @@ contract Presales is Ownable {
         uint256 paidAmount;
         uint256 tokenAmount;
         uint256 lastClaim;
-        bool isWL;
     }
     // allocation mapping per address
     mapping(address=>User) private users;
 
     modifier onlyWL {
-        require(users[msg.sender].isWL, "User is not whitelisted");
+        require(users[msg.sender].allocAmount > 0, "User is not whitelisted");
         _;
     }
 
@@ -64,66 +62,69 @@ contract Presales is Ownable {
         IERC20 presaleToken_,
         IERC20 paymentToken_,
         uint256 deadline_,
-        uint256 priNum_,
-        uint256 priDen_,
-        uint256 pubNum_,
-        uint256 pubDen_,
+        uint256 num_,
+        uint256 den_,
         uint256 vestedDuration_,
+        uint256 hardcap_,
         uint256 MAX_WALLET_) {
-            require(priDen_ > 0 && pubDen_ > 0, "Denominators must be greater than 0.");
+            require(den_ > 0 && den_ > 0, "Denominators must be greater than 0.");
             presaleToken = presaleToken_;
             paymentToken = paymentToken_;
             vestedDuration = vestedDuration_;
             deadline = deadline_;
             MAX_WALLET = MAX_WALLET_;
-            priDen = priDen_;
-            priNum = priNum_;
-            pubDen = pubDen_;
-            pubNum = pubNum_;
+            den = den_;
+            num = num_;
+            hardcap = hardcap_;
             receiver = msg.sender;
     }
 
-    function privateSale(uint256 amount_) external onlyWL {
+    function privateSale(uint256 amount_) external onlyWL nonReentrant {
+        require(currentCap.add(amount_) <= hardcap, "Hardcap has been reach.");
         require(block.timestamp < deadline, "Presale is closed.");
         require(amount_ > 0, "The minimum amount must be greater than 0.");
         require(users[msg.sender].allocAmount >= users[msg.sender].paidAmount.add(amount_), "You exceeded the authorized amount");
         paymentToken.transferFrom(msg.sender, address(this), amount_);
         users[msg.sender].paidAmount  += amount_;
-        users[msg.sender].tokenAmount += amount_.mul(priNum).div(priDen);
+        users[msg.sender].tokenAmount += amount_.mul(num).div(den);
         currentCap += amount_;
         emit PrivateSaleAttendance(msg.sender, amount_);
     }
 
     function publicSale(uint256 amount_) external {
-        require(block.timestamp >= deadline && block.timestamp < (deadline.add(86400)), "Public Sale is closed.");
-        require(users[msg.sender].paidAmount.add(amount_) > MAX_WALLET);
-        require(MAX_CAP.add(amount_) <= MAX_CAP);
+        require(isPublicSalesOpen, "Public Sale is closed.");
+        require(currentCap.add(amount_) <= hardcap, "Hardcap has been reach.");
+        require(users[msg.sender].paidAmount.mul(num).div(den) <= MAX_WALLET, "Max token per wallet reached.");
         paymentToken.transferFrom(msg.sender, address(this), amount_);
         users[msg.sender].paidAmount  += amount_;
-        users[msg.sender].tokenAmount += amount_.mul(pubNum).div(pubDen);
+        users[msg.sender].tokenAmount += amount_.mul(num).div(den);
         currentCap += amount_;
         emit PublicSaleAttendance(msg.sender, amount_);
     }
 
-    function claim() external {
+    function claim() external nonReentrant {
         require(isClaimOpen, "Claim is not open yet.");
         uint256 tokenAmount = getVestedAmountToClaim(users[msg.sender].tokenAmount);
+        
+        users[msg.sender].lastClaim = block.timestamp;
+        users[msg.sender].tokenAmount -= tokenAmount;
+
         presaleToken.approve(address(this), users[msg.sender].tokenAmount);
         presaleToken.transferFrom(
             address(this),
             msg.sender,
             tokenAmount
         );
-        users[msg.sender].lastClaim = block.timestamp;
-        users[msg.sender].tokenAmount -= tokenAmount;
+
     }
 
-    function refund() external {
+    function refund() external nonReentrant {
         require(isRFEnabled, "Refund is not enabled yet.");
         require(users[msg.sender].paidAmount > 0, "The minimum amount must be greater than 0.");
-        paymentToken.approve(address(this),users[msg.sender].paidAmount);
-        paymentToken.transferFrom(address(this), msg.sender,users[msg.sender].paidAmount);
-        users[msg.sender] = User(0,0,0,0,false);
+        uint256 paidAmount = users[msg.sender].paidAmount;
+        users[msg.sender] = User(0,0,0,0);
+        paymentToken.approve(address(this),paidAmount);
+        paymentToken.transferFrom(address(this), msg.sender,paidAmount);
     }
 
     // START: view functions
@@ -146,6 +147,7 @@ contract Presales is Ownable {
     function getVestedAmountToClaim(uint256 amount_) public view returns(uint256) {
         uint256 startTime = users[msg.sender].lastClaim;
         require(startTime < vestedTime, "you have no more to claim.");
+        require(vestedTime > 0, "math error: vestedTime must be greater than 0");
         if (startTime == 0) {
             startTime = launchtime;
         }
@@ -179,11 +181,8 @@ contract Presales is Ownable {
      * @param address_ Address of the user who will receive the allocation
      * @param amount_ Amount of the allocation
      */
-    function __updateWLAllocation(address address_, uint256 amount_, bool isWL_) external onlyOwner {
-        require(users[address_].allocAmount >= MAX_CAP, "alloc amount is lower than max cap");
-        MAX_CAP += amount_;
-        MAX_CAP -= users[address_].allocAmount;
-        users[address_] = User(amount_, 0,0,0,isWL_);
+    function __updateWLAllocation(address address_, uint256 amount_) external onlyOwner {
+        users[address_] = User(amount_, 0,0,0);
     }
 
     /**
@@ -193,8 +192,7 @@ contract Presales is Ownable {
      */
     function __addWLs(address[] memory addresses_, uint256 amount_) external onlyOwner {
         for(uint256 i=0;i<addresses_.length;i++){
-            users[addresses_[i]] = User(amount_, 0,0,0, true);
-            MAX_CAP += amount_;
+            users[addresses_[i]] = User(amount_, 0,0,0);
         }
     }
 
@@ -209,6 +207,12 @@ contract Presales is Ownable {
     function __setDeadline(uint256 deadline_) external onlyOwner {
         deadline = deadline_;
     }
+
+    function __setOpenPublicSales(bool isPublicSalesOpen_) 
+    external
+    onlyOwner {
+        isPublicSalesOpen = isPublicSalesOpen_;
+    }
     
     function __setIsClaimOpen(bool isClaimOpen_) external onlyOwner {
         isClaimOpen = isClaimOpen_;
@@ -218,6 +222,10 @@ contract Presales is Ownable {
     
     function __setVestedDuration(uint256 vestedDuration_) external onlyOwner {
         vestedDuration = vestedDuration_;
+    }
+
+    function __setHardcap(uint256 hardcap_) external onlyOwner {
+        hardcap = hardcap_;
     }
 
     function __setTokens(IERC20 paymentToken_, IERC20 presaleToken_)
